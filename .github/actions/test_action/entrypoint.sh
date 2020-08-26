fcb8c702cc49a28be1a5bc0fac86845b6299c2cb
Kurihara <kouji3211@fujitsu.com>
2020-08-26 23:25:39 +0900

    ci: add github action files

--- .github/actions/test_action/entrypoint.sh
+++ .github/actions/test_action/entrypoint.sh
@@ -16,8 +16,7 @@
 # *******************************************************************************/
 git submodule sync --recursive
 git submodule update --init --recursive
-pwd
-ls
+
 cd src/cpu/aarch64/xbyak_translator_aarch64/translator/third_party/
 mkdir build_xed_aarch64
 cd build_xed_aarch64/
@@ -29,8 +28,6 @@ cd xed/bin/
 CI_XED_PATH=`pwd`
 cd ../../../../../
 source dot.zshrc.xbyak
-export LD_LIBRARY_PATH=/github/workspace/src/cpu/aarch64/xbyak_translator_aarch64/translator/third_party/build_xed_aarch64/kits/xed/lib:${LD_LIBRARY_PATH}
-export XED_ROOT_DIR=/github/workspace/src/cpu/aarch64/xbyak_translator_aarch64/translator/third_party/build_xed_aarch64/kits/xed
 cd ../../../../../
 mkdir build
 cd build
@@ -38,6 +35,7 @@ export LD_LIBRARY_PATH=/github/workspace/src/cpu/aarch64/xbyak_translator_aarch6
 export XED_ROOT_DIR=/github/workspace/src/cpu/aarch64/xbyak_translator_aarch64/translator/third_party/build_xed_aarch64/kits/xed && \
 cmake -DCMAKE_BUILD_TYPE=Debug -DDNNL_INDIRECT_JIT_AARCH64=ON -DDNNL_TARGET_ARCH=AARCH64 -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=AARCH64 -DCMAKE_C_COMPILER=/usr/bin/aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=/usr/bin/aarch64-linux-gnu-g++  -DCMAKE_FIND_ROOT_PATH=/usr/aarch64-linux-gnu ..
 make -j2
+cd tests/gtests
 ./gtest_all.sh
 
 
