532542f98a1dd6f73c7ede1ff674cd4ac2cc9c54
m.yamazaki <m.yamazaki@fujitsu.com>
2020-09-21 16:10:45 +0900

    prevent Unknown CMake command "check_function_exists"

--- cmake/BLAS.cmake
+++ cmake/BLAS.cmake
@@ -41,6 +41,8 @@ SET(WITH_BLAS "" CACHE STRING "Blas type [ssl2/generic]")
 INCLUDE(CheckCSourceRuns)
 INCLUDE(CheckFortranFunctionExists)
 
+INCLUDE(CheckFunctionExists)
+
 MACRO(Check_Fortran_Libraries LIBRARIES _prefix _name _flags _list)
   # This macro checks for the existence of the combination of fortran libraries
   # given by _list.  If the combination is found, this macro checks (using the
