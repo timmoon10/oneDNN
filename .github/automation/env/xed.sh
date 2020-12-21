34fc2bd52be635fad404142552ced50466727f55
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-12-21 09:47:08 +0900

    build: add usage dump function

--- .github/automation/env/xed.sh
+++ .github/automation/env/xed.sh
@@ -14,31 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #===============================================================================
+#*******************************************************************************
+# Function definition
+#*******************************************************************************
+usage_exit() {
+    echo "Usage: $0 (-n|-q)"
+    echo "build libxed"
+    echo "  -n build libxed for native environment"
+    echo "  -q build libxed for qemu-aarch64 environment"
+    exit 1
+}
 
 # Build xed
 DIR_BUILD=src/cpu/aarch64/xbyak_translator_aarch64/translator/third_party/build_xed_aarch64
+OPT_CHECK=0
+USAGE_DONE=0
 mkdir -p ${DIR_BUILD}
 cd ${DIR_BUILD}
 
-while getopts nq OPT
+while getopts nqhH OPT
 do
     case $OPT in
-	q)
-	    ../xed/mfile.py \
-		--strip=/usr/bin/aarch64-linux-gnu-strip  \
-		--cc=/usr/bin/aarch64-linux-gnu-gcc \
-		--cxx=/usr/bin/aarch64-linux-gnu-g++ \
-		--host-cpu=aarch64 \
-		--shared examples install
-	    ;;
-	n)
-	    ../xed/mfile.py \
-	    --shared examples install
-	    ;;
+        q)
+            ../xed/mfile.py \
+                --strip=/usr/bin/aarch64-linux-gnu-strip  \
+                --cc=/usr/bin/aarch64-linux-gnu-gcc \
+                --cxx=/usr/bin/aarch64-linux-gnu-g++ \
+                --host-cpu=aarch64 \
+                --shared examples install
+            OPT_CHECK=1
+            ;;
+        n)
+            ../xed/mfile.py \
+            --shared examples install
+            OPT_CHECK=1
+            ;;
+        h|H)
+            usage_exit
+            USAGE_DONE=1
+            exit 1
+            ;;
    esac
 done
 shift $((OPTIND - 1))
 
+if [ ${OPT_CHECK} != 1 ] ; then
+    if [ ${USAGE_DONE} != 1 ] ; then
+        usage_exit
+    fi
+    exit 1
+fi
+
 cd kits
 ln -sf xed-install* xed
-
