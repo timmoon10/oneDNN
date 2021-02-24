f81c55d945e0abb7e3f817481ab5e6a8b0d86ff9
Kentaro Kawakami <kawakami.k@fujitsu.com>
2021-02-24 19:07:21 +0900

    aarch64: add profiler support

--- src/cpu/aarch64/jit_utils/jit_utils.cpp
+++ src/cpu/aarch64/jit_utils/jit_utils.cpp
@@ -1,6 +1,6 @@
 /*******************************************************************************
-* Copyright 2019-2020 Intel Corporation
-* Copyright 2020 FUJITSU LIMITED
+* Copyright 2019-2021 Intel Corporation
+* Copyright 2020-2021 FUJITSU LIMITED
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -19,8 +19,21 @@
 
 #include "common/utils.hpp"
 
+#ifndef DNNL_ENABLE_JIT_PROFILING
+#define DNNL_ENABLE_JIT_PROFILING 1
+#endif
+
 #ifndef DNNL_ENABLE_JIT_DUMP
 #define DNNL_ENABLE_JIT_DUMP 1
+#endif
+
+#if DNNL_ENABLE_JIT_PROFILING
+#include "cpu/aarch64/jit_utils/jitprofiling/jitprofiling.h"
+#ifdef __linux__
+#include "cpu/aarch64/jit_utils/linux_perf/linux_perf.hpp"
+#else
+#error "DNNL_ENABLE_JIT_PROFILING is only supported on linux"
+#endif
 #endif
 
 namespace dnnl {
@@ -41,7 +54,7 @@ void dump_jit_code(const void *code, size_t code_size, const char *code_name) {
 #define MAX_FNAME_LEN 256
         char fname[MAX_FNAME_LEN + 1];
         // TODO (Roma): support prefix for code / linux perf dumps
-        snprintf(fname, MAX_FNAME_LEN, "dnnl_dump_%s.%d.bin", code_name,
+        snprintf(fname, MAX_FNAME_LEN, "dnnl_dump_cpu_%s.%d.bin", code_name,
                 counter);
         counter++;
 
@@ -61,6 +74,22 @@ void dump_jit_code(const void *code, size_t code_size, const char *code_name) {
 #endif
 }
 
+void register_jit_code_linux_perf(const void *code, size_t code_size,
+        const char *code_name, const char *source_file_name) {
+#if DNNL_ENABLE_JIT_PROFILING && defined(__linux__)
+    unsigned flags = get_jit_profiling_flags();
+    if (flags & DNNL_JIT_PROFILE_LINUX_JITDUMP)
+        linux_perf_jitdump_record_code_load(code, code_size, code_name);
+    if (flags & DNNL_JIT_PROFILE_LINUX_PERFMAP)
+        linux_perf_perfmap_record_code_load(code, code_size, code_name);
+#else
+    UNUSED(code);
+    UNUSED(code_size);
+    UNUSED(code_name);
+#endif
+    UNUSED(source_file_name);
+}
+
 void register_jit_code(const void *code, size_t code_size,
         const char *code_name, const char *source_file_name) {
     // The #ifdef guards are required to avoid generating a function that only
@@ -71,6 +100,9 @@ void register_jit_code(const void *code, size_t code_size,
 
     dump_jit_code(
             code, code_size * sizeof(uint32_t) / sizeof(uint8_t), code_name);
+    register_jit_code_linux_perf(code,
+            code_size * sizeof(uint32_t) / sizeof(uint8_t), code_name,
+            source_file_name);
 #else
     UNUSED(code);
     UNUSED(code_size);
